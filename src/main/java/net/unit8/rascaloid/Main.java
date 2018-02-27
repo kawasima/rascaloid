9950b70ccc4379e1d7f8871abbfcb7dd5947adaa
kawasima <kawasima1016@gmail.com>
2018-02-27 09:46:57 +0900

    When main class has been started, start EnkanSystem

--- src/main/java/net/unit8/rascaloid/Main.java
+++ src/main/java/net/unit8/rascaloid/Main.java
@@ -1,16 +1,20 @@
 package net.unit8.rascaloid;
 
+import enkan.system.EnkanSystem;
 import enkan.system.command.JsonRequestCommand;
 import enkan.system.command.MetricsCommandRegister;
 import enkan.system.command.SqlCommand;
 import enkan.system.repl.PseudoRepl;
 import enkan.system.repl.ReplBoot;
-import enkan.system.repl.client.ReplClient;
 import kotowari.system.KotowariCommandRegister;
 
+import java.lang.reflect.Field;
+
+import static enkan.util.ReflectionUtils.tryReflection;
+
 public class Main {
-    public static void main(String[] args) throws Exception {
-        PseudoRepl repl = new PseudoRepl("net.unit8.rascaloid.RascaloidSystemFactory");
+    public static void main(String[] args) {
+        final PseudoRepl repl = new PseudoRepl("net.unit8.rascaloid.RascaloidSystemFactory");
         ReplBoot.start(repl,
             new KotowariCommandRegister(),
             r -> {
@@ -19,6 +23,12 @@ public class Main {
             },
             new MetricsCommandRegister());
 
-        new ReplClient().start(repl.getPort());
+        tryReflection(() -> {
+            Field f = PseudoRepl.class.getDeclaredField("system");
+            f.setAccessible(true);
+            EnkanSystem system = (EnkanSystem) f.get(repl);
+            system.start();
+            return true;
+        });
     }
 }
